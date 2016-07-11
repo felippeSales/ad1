2ec1e573ef6706c92824d9b8d334a9814cf9a25c
Felipe Sales <sales.felipeb@gmail.com>
2016-07-11 10:50:36 -0300

    init

--- problema-1/problema1.R
+++ problema-1/problema1.R
@@ -1,24 +1,37 @@
 library(ggplot2)
 library(dplyr)
 
-## Problema 1 
 
 ## Analisando os gastos comp passagens aéreas
 
+## Selecionando apenas os gastos com passagens aéreas:
+##gastos.passagens <- subset(ano.atual,txtDescricao == "Emissão Bilhete Aéreo")
+
+## Problema 1 
+
 ano.atual <- read.csv("~/Documents/workspace/UFCG/AD1-2016.1/repo/problema-1/dados/ano-atual.csv")
 
-## Selecionando apenas os gastos com passagens aéreas:
-
-gastos.passagens <- subset(ano.atual,txtDescricao == "Emissão Bilhete Aéreo")
-
-summary(gastos.passagens$vlrLiquido)
-
-media.categorias <-group_by(ano.atual, txtDescricao, add = TRUE)
 
 
-  
-##media.categorias.bar <- ggplot(media.categorias, aes(factor(Group.1), vlrLiquido)) + 
- ## geom_bar(stat="identity", position = "dodge")
+##Panorama dos dados
 
-#media.categorias.bar + coord_flip()
+
+## Analisando gastos por tipo
+
+vlrLiquidoAgrupadosDescricao <- ano.atual %>% group_by(txtDescricao) %>% summarise(vlrLiquido = sum(vlrLiquido))
+
+ggplot(vlrLiquidoAgrupadosDescricao, aes(factor(txtDescricao), vlrLiquido)) + 
+geom_bar(stat="identity", position = "dodge") + coord_flip()
+
+## Analisando gastos por partido
+
+vlrLiquidoAgrupadosPartido <- ano.atual %>% group_by(sgPartido) %>% summarise(vlrLiquido = mean(vlrLiquido))
+
+ggplot(vlrLiquidoAgrupadosPartido, aes(factor(sgPartido), vlrLiquido)) + 
+geom_bar(stat="identity", position = "dodge") + coord_flip()
+
+
+p <- ggplot(ano.atual, aes(datEmissao, vlrLiquido))
+p + geom_point()
+
 
